8d5345c77e226ff6fa9fa7701ff634082816e1c5
matias cardoso <mattiascardoso10@gmail.com>
2022-10-18 13:35:52 -0300

    agregamos validaciones al formulario

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,13 +1,18 @@
 const URL = process.env.REACT_APP_API_CAFE;
+// peticion GET para obtener el arreglo de productos o un producto
+// peticion POST para crear un producto (generalmente en formato JSON), tambien se usa en el login
+// peticion PUT para modificar un producto
+// peticion DELETE para borrar un producto
+
 
 export const consultarApi=async()=>{
     try {
-        // peticion get para obtener el arreglo
         const respuesta = await fetch(URL);
         const listaProductos =await respuesta.json()
         return listaProductos;
     } catch (error) {
         console.log(error);
+        return false;
     }
 }
 
